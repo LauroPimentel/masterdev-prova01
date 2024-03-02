1820f9fcb4f182e6d490d707934608bdfa94b25d
LAURO GABRIEL DA SILVA PIMENTEL <LAURO0880@DIGITALCOLLEGE.LOCAL>
2024-03-02 14:21:39 -0300

    Desafio

--- src/marminino.js
+++ src/marminino.js
@@ -1,5 +1,16 @@
 function executar(numero) {
-    // yourcode
+    let resposta = "";
+    if (numero % 3 === 0) { 
+        resposta += "Mar";
+    }
+    if (numero % 4 === 0) {
+        resposta += "Minino";
+    }
+    return resposta;
 }
+for (let i = 1; i <= 100; i++) {
+    console.log(i + ": " + executar(i));
+}
+
 
 module.exports = executar;
